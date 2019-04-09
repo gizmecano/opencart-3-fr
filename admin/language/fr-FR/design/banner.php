f9329e1d0ca04f682fc43f78788b264d376bd7d5
P. Mergey <gzm@gizmecano.net>
2019-04-09 10:50:07 +0200

    Add </admin/design/banner.php>

---
+++ admin/language/fr-FR/design/banner.php
@@ -0,0 +1,37 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Bannières';
+// Text
+$_['text_success'] = 'Les bannières ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des bannières';
+$_['text_add'] = 'Ajouter une bannière';
+$_['text_edit'] = 'Modifier une bannière';
+$_['text_default'] = 'Par défaut';
+// Column
+$_['column_name'] = 'Nom de la bannière';
+$_['column_status'] = 'Statut';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom de la bannière';
+$_['entry_title'] = 'Titre';
+$_['entry_link'] = 'Lien';
+$_['entry_image'] = 'Image';
+$_['entry_status'] = 'Statut';
+$_['entry_sort_order'] = 'Classement';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les bannières !';
+$_['error_name'] = 'Le nom de la bannière doit contenir entre 3 et 64 caractères !';
+$_['error_title'] = 'Le titre de la bannière doit contenir entre 2 et 64 caractères !';
