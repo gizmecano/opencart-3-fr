07cd08b06cd7914dbe642fc63fe50fb2dc545dc4
P. Mergey <gzm@gizmecano.net>
2019-04-09 11:10:36 +0200

    Add </admin/design/translation.php>

---
+++ admin/language/fr-FR/design/translation.php
@@ -0,0 +1,41 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Éditeur linguistique';
+// Text
+$_['text_success'] = 'L’éditeur linguistique a été modifié avec succès !';
+$_['text_list'] = 'Liste de traduction';
+$_['text_edit'] = 'Modifier la traduction';
+$_['text_add'] = 'Ajouter une traduction';
+$_['text_default'] = 'Par défaut';
+$_['text_store'] = 'Boutique';
+$_['text_language'] = 'Langue';
+// Column
+$_['column_store'] = 'Boutique';
+$_['column_language'] = 'Langue';
+$_['column_route'] = 'Chemin';
+$_['column_key'] = 'Clef';
+$_['column_value'] = 'Valeur';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_store'] = 'Boutique';
+$_['entry_language'] = 'Langue';
+$_['entry_route'] = 'Chemin';
+$_['entry_key'] = 'Clef';
+$_['entry_value'] = 'Valeur';
+$_['entry_default'] = 'Par défaut';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier l’éditeur linguistique !';
+$_['error_key'] = 'Une clef doit contenir entre 3 et 64 caractères !';
