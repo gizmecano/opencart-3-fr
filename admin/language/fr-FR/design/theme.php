32f7801545ba09e3d262032c1ff3a9ab1ae74de8
P. Mergey <gzm@gizmecano.net>
2019-04-09 10:45:56 +0200

    Add </admin/design/theme.php>

---
+++ admin/language/fr-FR/design/theme.php
@@ -0,0 +1,33 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Éditeur de thème';
+// Text
+$_['text_success'] = 'Le thème a été modifié avec succès !';
+$_['text_edit'] = 'Modifier le thème';
+$_['text_store'] = 'Choisir la boutique';
+$_['text_template'] = 'Choisir le modèle';
+$_['text_default'] = 'Par défaut';
+$_['text_history'] = 'Historique du thème';
+$_['text_twig'] = 'L’éditeur de thème utilise le langage Twig. Vous pouvez trouver des renseignements sur <a href="http://twig.sensiolabs.org/documentation" target="_blank" class="alert-link">la syntaxe du langage Twig ici</a>.';
+// Column
+$_['column_store'] = 'Boutique';
+$_['column_route'] = 'Chemin';
+$_['column_theme'] = 'Thème';
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_action'] = 'Action';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier l’éditeur de thème !';
+$_['error_twig'] = 'Attention : vous pouvez seulement sauvegarder des fichiers au format .twig !';
