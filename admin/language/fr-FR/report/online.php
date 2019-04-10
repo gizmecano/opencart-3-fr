bfe7dad9801525114961b538ac8d6c1f0573c408
P. Mergey <gzm@gizmecano.net>
2019-04-10 10:28:16 +0200

    Add </admin/report/online.php>

---
+++ admin/language/fr-FR/report/online.php
@@ -0,0 +1,36 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Rapport des clients en ligne';
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Le rapport des clients en ligne a été modifié avec succès !';
+$_['text_list'] = 'Liste des clients en ligne';
+$_['text_filter'] = 'Filtre';
+$_['text_guest'] = 'Invités';
+// Column
+$_['column_ip'] = 'IP';
+$_['column_customer'] = 'Client';
+$_['column_url'] = 'Dernière page visitée';
+$_['column_referer'] = 'Site référent';
+$_['column_date_added'] = 'Dernier clic';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_ip'] = 'IP';
+$_['entry_customer'] = 'Client';
+$_['entry_status'] = 'Statut';
+$_['entry_sort_order'] = 'Classement';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier le rapport des clients en ligne !';
