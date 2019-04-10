17490f206d1cd367c0247929c9370668a9866aaf
P. Mergey <gzm@gizmecano.net>
2019-04-10 10:49:06 +0200

    Add </admin/tool/backup.php>

---
+++ admin/language/fr-FR/tool/backup.php
@@ -0,0 +1,28 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Sauvegarde et restauration';
+// Text
+$_['text_success'] = 'La base de données a été sauvegardée avec succès !';
+// Entry
+$_['entry_progress']= 'Progression';
+$_['entry_export'] = 'Exportation';
+// Tab
+$_['tab_backup']= 'Sauvegarde';
+$_['tab_restore']= 'Restauration';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les sauvegardes et restaurations !';
+$_['error_export'] = 'Attention : vous devez sélectionner au moins une table à exporter !';
+$_['error_file']= 'Le fichier est introuvable !';
