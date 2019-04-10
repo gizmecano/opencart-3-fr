ac3add88dc90248bdd244db774e31f515d06e058
P. Mergey <gzm@gizmecano.net>
2019-04-10 11:25:08 +0200

    Add </admin/user/user.php>

---
+++ admin/language/fr-FR/user/user.php
@@ -0,0 +1,47 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Utilisateurs';
+// Text
+$_['text_success'] = 'Les utilisateurs ont été modifiés avec succès !';
+$_['text_list'] = 'Liste des utilisateurs';
+$_['text_add'] = 'Ajouter un utilisateur';
+$_['text_edit'] = 'Modifier un utilisateur';
+// Column
+$_['column_username'] = 'Nom d’utilisateur';
+$_['column_status'] = 'Statut';
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_username'] = 'Nom d’utilisateur';
+$_['entry_user_group'] = 'groupe d’utilisateurs';
+$_['entry_password'] = 'Mot de passe';
+$_['entry_confirm'] = 'Confirmer';
+$_['entry_firstname'] = 'Prénom';
+$_['entry_lastname'] = 'Nom de famille';
+$_['entry_email'] = 'Adresse électronique';
+$_['entry_image'] = 'Image';
+$_['entry_status'] = 'Statut';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les utilisateurs !';
+$_['error_account'] = 'Attention : vous ne pouvez pas supprimer votre propre compte !';
+$_['error_exists_username'] = 'Attention : ce nom d’utilisateur est déjà utilisé !';
+$_['error_username'] = 'Le nom d’utilisateur doit contenir entre 3 et 20 caractères !';
+$_['error_password'] = 'Le mot de passe doit contenir entre 4 et 20 caractères !';
+$_['error_confirm'] = 'Le mot de passe de confirmation ne correspond pas au premier mot de passe !';
+$_['error_firstname'] = 'Le prénom doit contenir entre 1 à 32 caractères !';
+$_['error_lastname'] = 'Le nom de famille doit contenir entre 1 à 32 caractères !';
+$_['error_email'] = 'Attention : cette adresse électronique ne semble pas être valide !';
+$_['error_exists_email'] = 'Attention : cette adresse électronique est déjà enregistrée !';
