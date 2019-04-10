5b9f9909aaf8a92997d166ba0bd9c4f3a7949138
P. Mergey <gzm@gizmecano.net>
2019-04-10 11:10:09 +0200

    Add </catalog/common/header.php>

---
+++ catalog/language/fr-FR/common/header.php
@@ -0,0 +1,29 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_home'] = 'Accueil';
+$_['text_wishlist'] = 'Liste de souhaits (%s)';
+$_['text_shopping_cart'] = 'Panier d’achat';
+$_['text_category'] = 'Catégories';
+$_['text_account'] = 'Mon compte';
+$_['text_register'] = 'Inscription';
+$_['text_login'] = 'Ouvrir une session';
+$_['text_order'] = 'Historique des commandes';
+$_['text_transaction'] = 'Transactions';
+$_['text_download'] = 'Téléchargements';
+$_['text_logout'] = 'Se déconnecter';
+$_['text_checkout'] = 'Valider la commande';
+$_['text_search'] = 'Rechercher';
+$_['text_all'] = 'Afficher tout';
