20f7ab76de53d8fcda3dd621223b3333a0aa8025
P. Mergey <gzm@gizmecano.net>
2019-04-10 10:40:15 +0200

    Add </admin/report/statistics.php>

---
+++ admin/language/fr-FR/report/statistics.php
@@ -0,0 +1,34 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Statistiques';
+// Text
+$_['text_success'] = 'Les statistiques ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des statistiques';
+$_['text_order_sale'] = 'Commandes vendues';
+$_['text_order_processing'] = 'Commandes en cours';
+$_['text_order_complete'] = 'Commandes complètes';
+$_['text_order_other'] = 'Autres statuts de commande';
+$_['text_returns'] = 'Retours';
+$_['text_customer'] = 'Clients en attente d’approbation';
+$_['text_affiliate'] = 'Affiliés en attente d’approbation';
+$_['text_product'] = 'Produits indisponibles';
+$_['text_review'] = 'Évaluations en attente';
+// Column
+$_['column_name'] = 'Nom de la statistique';
+$_['column_value'] = 'Valeur';
+$_['column_action'] = 'Action';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les statistiques !';
