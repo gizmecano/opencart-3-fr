8ad3ee120062d309fbc0ace45fba886dfee6a7d0
P. Mergey <gzm@gizmecano.net>
2019-04-10 10:10:57 +0200

    Add </admin/common/filemanager.php>

---
+++ admin/language/fr-FR/common/filemanager.php
@@ -0,0 +1,33 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Gestionnaire d’images';
+// Text
+$_['text_uploaded'] = 'Le fichier a été transféré avec succès !';
+$_['text_directory'] = 'Le dossier a été créé !';
+$_['text_delete'] = 'Le fichier ou dossier a été supprimé correctement !';
+// Entry
+$_['entry_search'] = 'Recherche...';
+$_['entry_folder'] = 'Nom du dossier';
+// Error
+$_['error_permission'] = 'Attention : accès refusé !';
+$_['error_filename'] = 'Attention : le nom du fichier doit contenir entre 3 et 255 caractères !';
+$_['error_folder'] = 'Attention : le nom du dossier doit contenir entre 3 et 255 caractères !';
+$_['error_exists'] = 'Attention : un fichier ou dossier portant le même nom existe déjà !';
+$_['error_directory'] = 'Attention : le dossier n’existe pas !';
+$_['error_filesize'] = 'Attention : la taille de fichier est incorrecte !';
+$_['error_filetype'] = 'Attention : le type de fichier est incorrect !';
+$_['error_upload'] = 'Attention : le fichier n’a pas pu être transféré pour une raison inconnue !';
+$_['error_delete'] = 'Attention : vous ne pouvez pas supprimer ce dossier !';
