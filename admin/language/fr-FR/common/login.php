7e731c5eba5fafb55a3a508503857973741f1f62
P. Mergey <gzm@gizmecano.net>
2019-04-11 10:42:01 +0200

    Add </admin/common/login.php>

---
+++ admin/language/fr-FR/common/login.php
@@ -0,0 +1,28 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Administration';
+// Text
+$_['text_heading'] = 'Administration';
+$_['text_login'] = 'Veuillez saisir vos identifiants.';
+$_['text_forgotten'] = 'Mot de passe oublié';
+// Entry
+$_['entry_username'] = 'Nom d’utilisateur';
+$_['entry_password'] = 'Mot de passe';
+// Button
+$_['button_login'] = 'Ouvrir une session';
+// Error
+$_['error_login'] = 'Attention : correspondance introuvable entre ce nom d’utilisateur et ce mot de passe.';
+$_['error_token'] = 'Attention : session invalide. Veuillez ouvrir une nouvelle session.';
