dcf391e220cc6f5026eabb00a83d32f4ccc0afc2
P. Mergey <gzm@gizmecano.net>
2019-04-11 11:38:10 +0200

    Add </admin/localisation/return_reason.php>

---
+++ admin/language/fr-FR/localisation/return_reason.php
@@ -0,0 +1,30 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Motifs de retour';
+// Text
+$_['text_success'] = 'Les motifs de retour ont été modifiés avec succès !';
+$_['text_list'] = 'Liste des motifs de retour';
+$_['text_add'] = 'Ajouter un motif de retour';
+$_['text_edit'] = 'Modifier un motif de retour';
+// Column
+$_['column_name'] = 'Motif de retour';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom du motif de retour';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les motifs de retour !';
+$_['error_name'] = 'Le nom du motif de retour doit contenir entre 3 et 128 caractères !';
+$_['error_return'] = 'Attention : ce motif de retour ne peut pas être supprimé car il est actuellement attribué à %s produits retournés !';
