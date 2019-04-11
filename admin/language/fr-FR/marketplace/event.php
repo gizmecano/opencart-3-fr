1bc5ea9f5494231aaeb15c9878426f00fcd7d614
P. Mergey <gzm@gizmecano.net>
2019-04-11 09:40:07 +0200

    Add </admin/marketplace/event.php>

---
+++ admin/language/fr-FR/marketplace/event.php
@@ -0,0 +1,30 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Événements';
+// Text
+$_['text_success'] = 'Les événements ont été modifiés avec succès !';
+$_['text_list'] = 'Liste d’événements';
+$_['text_event'] = 'Les événements sont utilisés par les extensions pour remplacer des fonctionnalités par défaut de votre boutique. Si vous rencontrez des problèmes, vous pouvez activer ou désactiver les événements ici.';
+$_['text_info'] = 'Information sur l’événement';
+$_['text_trigger'] = 'Déclencheur';
+$_['text_action'] = 'Action';
+// Column
+$_['column_code'] = 'Code de l’événement';
+$_['column_status'] = 'Statut';
+$_['column_sort_order'] = 'Classement';
+$_['column_action'] = 'Action';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les événements !';
