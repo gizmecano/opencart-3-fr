c1c625f4c9a226c2b4567a4dd6e077639be5e7bf
P. Mergey <gzm@gizmecano.net>
2019-04-11 11:00:36 +0200

    Add </admin/localisation/geo_zone.php>

---
+++ admin/language/fr-FR/localisation/geo_zone.php
@@ -0,0 +1,36 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Zones géographiques';
+// Text
+$_['text_success'] = 'Les zones géographiques ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des zones géographiques';
+$_['text_add'] = 'Ajouter une zone géographique';
+$_['text_edit'] = 'Modifier une zone géographique';
+$_['text_geo_zone'] = 'Zones géographiques';
+// Column
+$_['column_name'] = 'Nom de la zone géographique';
+$_['column_description'] = 'Description';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom de la zone géographique';
+$_['entry_description'] = 'Description';
+$_['entry_country'] = 'Pays';
+$_['entry_zone'] = 'Zone';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les zones géographiques !';
+$_['error_name'] = 'Le nom de la zone géographique doit contenir entre 3 et 32 caractères !';
+$_['error_description'] = 'La description de la zone géographique doit contenir entre 3 et 255 caractères !';
+$_['error_tax_rate'] = 'Attention : cette zone géographique ne peut pas être supprimée car elle est actuellement attribuée à un ou plusieurs taux de taxe !';
