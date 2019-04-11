46f553fa27e704ac64bf676fc86f230f338622b4
P. Mergey <gzm@gizmecano.net>
2019-04-11 09:20:04 +0200

    Add </admin/marketplace/install.php>

---
+++ admin/language/fr-FR/marketplace/install.php
@@ -0,0 +1,30 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_success'] = 'Les extensions ont été modifiées avec succès !';
+$_['text_unzip'] = 'Extraction des fichiers !';
+$_['text_move'] = 'Copie des fichiers !';
+$_['text_xml'] = 'Application des changements !';
+$_['text_remove'] = 'Suppression des fichiers temporaires !';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les extensions !';
+$_['error_install'] = 'Installation d’extension en cours, veuillez patienter quelques secondes avant d’essayer de l’installer !';
+$_['error_unzip'] = 'Le fichier ZIP n’a pas pu être ouvert !';
+$_['error_file'] = 'Le fichier d’installation est introuvable !';
+$_['error_directory'] = 'Le répertoire d’installation est introuvable !';
+$_['error_code'] = 'La modification XML nécessite un code d’identification unique !';
+$_['error_xml'] = 'La modification %s est déjà utilisée !';
+$_['error_exists'] = 'Le fichier %s existe déjà !';
+$_['error_allowed'] = 'Le répertoire % s n’est pas autorisé en écriture !';
