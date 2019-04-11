4f950f2639e1019f414c191cba98d2ad30faed7f
P. Mergey <gzm@gizmecano.net>
2019-04-11 11:28:02 +0200

    Add </admin/localisation/return_status.php>

---
+++ admin/language/fr-FR/localisation/return_status.php
@@ -0,0 +1,31 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Statuts des retours';
+// Text
+$_['text_success'] = 'Les statuts de retour ont été modifiés avec succès !';
+$_['text_list'] = 'Liste des statuts de retour';
+$_['text_add'] = 'Ajouter un statut de retour';
+$_['text_edit'] = 'Modifier un statut de retour';
+// Column
+$_['column_name'] = 'Nom du statut de retour';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom du statut de retour';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les statuts de retour !';
+$_['error_name'] = 'Le nom du statut de retour doit contenir entre 3 et 32 caractères !';
+$_['error_default'] = 'Attention : ce statut de retour ne peut pas être supprimé car il est actuellement assigné comme statut de retour par défaut !';
+$_['error_return'] = 'Attention : ce statut de retour ne peut pas être supprimé car il est actuellement attribué à %s retours !';
