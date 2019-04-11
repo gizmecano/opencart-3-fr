5315452316bbb25460c5214c0fbad31896081fe8
P. Mergey <gzm@gizmecano.net>
2019-04-11 09:36:54 +0200

    Add </admin/marketplace/marketplace.php>

---
+++ admin/language/fr-FR/marketplace/marketplace.php
@@ -0,0 +1,80 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Cybermarché des extensions';
+// Text
+$_['text_success'] = 'Les extensions ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des extensions';
+$_['text_filter'] = 'Filtre';
+$_['text_search'] = 'Rechercher des extensions et des thèmes';
+$_['text_category'] = 'Catégories';
+$_['text_all'] = 'Tout';
+$_['text_theme'] = 'Thèmes';
+$_['text_marketplace'] = 'Cybermarchés';
+$_['text_language'] = 'Langues';
+$_['text_payment'] = 'Paiement';
+$_['text_shipping'] = 'Livraison';
+$_['text_module'] = 'Modules';
+$_['text_total'] = 'Totaux de commande';
+$_['text_feed'] = 'Flux';
+$_['text_report'] = 'Rapports';
+$_['text_other'] = 'Autre';
+$_['text_free'] = 'Gratuit';
+$_['text_paid'] = 'Payé';
+$_['text_purchased'] = 'Acheté';
+$_['text_date_modified'] = 'Date de modification';
+$_['text_date_added'] = 'Date d’ajout';
+$_['text_rating'] = 'Notes';
+$_['text_reviews'] = 'Évaluations';
+$_['text_compatibility'] = 'Compatibilité';
+$_['text_downloaded'] = 'Téléchargé';
+$_['text_member_since'] = 'Membre depuis : :';
+$_['text_price'] = 'Prix';
+$_['text_partner'] = 'Développé par un partenaire d’OpenCart';
+$_['text_support'] = '12 mois de support gratuit';
+$_['text_documentation'] = 'Documentation incluse';
+$_['text_sales'] = 'Ventes';
+$_['text_comment'] = 'Commentaires';
+$_['text_download'] = 'Téléchargement en cours';
+$_['text_install'] = 'Installation en cours';
+$_['text_comment_add'] = 'Laisser votre commentaire';
+$_['text_write'] = 'Rédiger votre commentaire ici :';
+$_['text_purchase'] = 'Veuillez confirmer votre identité !';
+$_['text_pin'] = 'Veuillez saisir votre numéro PIN à 4 chiffres. Ce code PIN est conçu pour protéger votre compte.';
+$_['text_secure'] = 'Ne transmettez pas votre numéro PIN à qui que ce soit, y compris les développeurs. Si vous avez besoin qu’un vendeur d’extensions en installe une pour vous, vous devriez envoyer par courrier électronique l’extension requise.';
+$_['text_name'] = 'Nom du téléchargement';
+$_['text_progress'] = 'Progression';
+$_['text_available'] = 'Installations disponibles';
+$_['text_action'] = 'Action';
+// Entry
+$_['entry_pin'] = 'PIN';
+// Tab
+$_['tab_description'] = 'Description';
+$_['tab_documentation'] = 'Documentation';
+$_['tab_download'] = 'Téléchargement';
+$_['tab_comment'] = 'Commentaire';
+// Button
+$_['button_opencart'] = 'API du Cybermarché OpenCart';
+$_['button_purchase'] = 'Acheter';
+$_['button_view_all'] = 'Voir toutes les extensions';
+$_['button_get_support'] = 'Obtenir l’aide du support';
+$_['button_comment'] = 'Commentaire';
+$_['button_reply'] = 'Répondre';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les extensions !';
+$_['error_opencart'] = 'Attention : vous devez saisir vos informations de l’API du Cybermarché OpenCart avant de pouvoir faire tout achat !';
+$_['error_install'] = 'Installation d’extension en cours, veuillez patienter quelques secondes avant d’essayer de l’installer !';
+$_['error_purchase'] = 'L’extension ne peut pas être achetée !';
+$_['error_download'] = 'L’extension ne peut pas être téléchargée !';
