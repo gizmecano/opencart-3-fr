79ae3482bea485796ca21f6e482721e024d84740
P. Mergey <gzm@gizmecano.net>
2019-04-11 10:13:05 +0200

    Add </admin/marketing/contact.php>

---
+++ admin/language/fr-FR/marketing/contact.php
@@ -0,0 +1,45 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Lettre d’informations';
+// Text
+$_['text_success'] = 'Votre lettre d’informations a été envoyée avec succès !';
+$_['text_sent'] = 'Votre lettre d’informations a bien été transmise à %s sur %s destinataires !';
+$_['text_list'] = 'Liste des gabarits';
+$_['text_default'] = 'Par défaut';
+$_['text_newsletter'] = 'Tous les abonnés à la lettre d’informations';
+$_['text_customer_all'] = 'Tous les clients';
+$_['text_customer_group'] = 'Groupe de clients';
+$_['text_customer'] = 'Clients';
+$_['text_affiliate_all'] = 'Tous les affiliés';
+$_['text_affiliate'] = 'Affiliés';
+$_['text_product'] = 'Produits';
+// Entry
+$_['entry_store'] = 'De';
+$_['entry_to'] = 'À';
+$_['entry_customer_group'] = 'Groupe de clients';
+$_['entry_customer'] = 'Client';
+$_['entry_affiliate'] = 'Affilié';
+$_['entry_product'] = 'Produits';
+$_['entry_subject'] = 'Sujet';
+$_['entry_message'] = 'Message';
+// Help
+$_['help_customer'] = '(Autocomplétion)';
+$_['help_affiliate'] = '(Autocomplétion)';
+$_['help_product'] = 'Envoyer uniquement aux clients qui ont commandé des produits dans la liste. Autocomplétion';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation d’envoyer une lettre d’informations !';
+$_['error_subject'] = 'Le sujet de la lettre d’informations est requis !';
+$_['error_message'] = 'Le corps de la lettre d’informations est requis !';
