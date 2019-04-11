85b986d6983a5c731bece160e2d99619dae22f16
P. Mergey <gzm@gizmecano.net>
2019-04-11 08:06:54 +0200

    Add </admin/catalog/attribute_group.php>

---
+++ admin/language/fr-FR/catalog/attribute_group.php
@@ -0,0 +1,33 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Groupe d’attributs';
+// Text
+$_['text_success'] = 'Les groupes d’attributs ont été modifiés avec succès !';
+$_['text_list'] = 'Liste des groupes d’attributs';
+$_['text_add'] = 'Ajouter un groupe d’attributs';
+$_['text_edit'] = 'Modifier un groupe d’attributs';
+// Column
+$_['column_name'] = 'Nom du groupe d’attributs';
+$_['column_sort_order'] = 'Classement';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom du groupe d’attributs';
+$_['entry_sort_order'] = 'Classement';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les groupes d’attributs !';
+$_['error_name'] = 'Attention : le nom du groupe d’attributs doit contenir entre 3 et 64 caractères !';
+$_['error_attribute'] = 'Attention : ce groupe d’attributs ne peut pas être supprimé car il est actuellement attribué à %s attributs !';
+$_['error_product'] = 'Attention : ce groupe d’attributs ne peut pas être supprimé car il est actuellement attribué à %s produits !';
