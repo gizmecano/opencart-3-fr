863c0ddc60e8139d0a1cb489eb73b04228983fc7
P. Mergey <gzm@gizmecano.net>
2019-04-11 11:02:13 +0200

    Add </admin/localisation/zone.php>

---
+++ admin/language/fr-FR/localisation/zone.php
@@ -0,0 +1,39 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Zones';
+// Text
+$_['text_success'] = 'Les zones ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des zones';
+$_['text_add'] = 'Ajouter une zone';
+$_['text_edit'] = 'Modifier une zone';
+// Column
+$_['column_name'] = 'Zone';
+$_['column_code'] = 'Code';
+$_['column_country'] = 'Pays';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom de la zone';
+$_['entry_code'] = 'Code de la zone';
+$_['entry_country'] = 'Pays';
+$_['entry_status'] = 'État de la zone';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les zones !';
+$_['error_name'] = 'Le nom de la zone doit contenir entre 3 et 128 caractères !';
+$_['error_default'] = 'Attention : cette zone ne peut pas être supprimée car elle est actuellement définie en tant que domaine par défaut !';
+$_['error_store'] = 'Attention : cette zone ne peut pas être supprimée car elle est actuellement attribuée à %s boutiques !';
+$_['error_address'] = 'Attention : cette zone ne peut pas être supprimée car elle est actuellement attribuée à %s entrées du carnet d’adresses !';
+$_['error_affiliate'] = 'Attention : cette zone ne peut pas être supprimée car elle est actuellement attribuée à %s affiliés !';
+$_['error_zone_to_geo_zone'] = 'Attention : cette zone ne peut pas être supprimée car elle est actuellement attribuée à %s zones géographiques !';
