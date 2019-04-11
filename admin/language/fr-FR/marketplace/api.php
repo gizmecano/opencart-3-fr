1e42f121982d160ff970c33b8a0bc1169ac2de40
P. Mergey <gzm@gizmecano.net>
2019-04-11 09:30:37 +0200

    Add </admin/marketplace/api.php>

---
+++ admin/language/fr-FR/marketplace/api.php
@@ -0,0 +1,26 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'API du Cybermarché OpenCart';
+// Text
+$_['text_success'] = 'Les informations de l’API du Cybermarché OpenCart ont été modifiées avec succès !';
+$_['text_signup'] = 'Veuillez saisir vos informations de l’API du Cybermarché OpenCart que vous pouvez obtenir <a href="https://www.opencart.com/index.php?route=account/store" target="_blank" class="alert-link">ici</a>.';
+// Entry
+$_['entry_username'] = 'Nom d’utilisateur';
+$_['entry_secret'] = 'Secret';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les informations de l’API du Cybermarché OpenCart !';
+$_['error_username'] = 'Nom d’utilisateur requis !';
+$_['error_secret'] = 'Secret requis !';
