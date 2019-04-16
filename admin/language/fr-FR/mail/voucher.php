30c134534dec9f873bae06f0e2925083577b4d31
P. Mergey <gzm@gizmecano.net>
2019-04-16 15:22:09 +0200

    Add </admin/mail/voucher.php>

---
+++ admin/language/fr-FR/mail/voucher.php
@@ -0,0 +1,22 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_success'] = 'Les bons d’achat ont été modifiés avec succès !';
+$_['text_subject'] = 'Vous venez de recevoir un bon d’achat provenant de %s';
+$_['text_greeting'] = 'Vous venez de recevoir avec succès un bon d’achat d’une valeur de %s';
+$_['text_from'] = 'Ce bon d’achat a été envoyé de la part de %s';
+$_['text_message'] = 'Avec le message suivant';
+$_['text_redeem'] = 'Pour échanger ce bon d’achat, notez bien le code de rachat qui est <b>%s</b>. Cliquer ensuite sur le lien ci-dessous et acheter le produit pour lequel vous souhaitez utiliser ce bon d’achat. Vous pouvez saisir le code du bon d’achat sur la page du panier avant de valider la commande.';
+$_['text_footer'] = 'Vous pouvez répondre à ce courrier dans le cas où vous auriez une question.';
