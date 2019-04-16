fd50376fdaae73436fcf19d3e96ee04016de2f24
P. Mergey <gzm@gizmecano.net>
2019-04-16 14:14:10 +0200

    Add </admin/mail/affiliate_approve.php>

---
+++ admin/language/fr-FR/mail/affiliate_approve.php
@@ -0,0 +1,20 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_subject'] = '%s - Votre compte d’affilié a été activé !';
+$_['text_welcome'] = 'Bienvenue et merci pour votre enregistrement à %s !';
+$_['text_login'] = 'Votre compte a été approuvé et vous pouvez vous connecter en utilisant votre adresse électronique et votre mot de passe sur notre site internet ou à l’adresse suivante :';
+$_['text_services'] = 'Une fois votre connexion établie, vous serez en mesure de générer des codes de suivi, suivre le paiement des commissions et modifier les informations de votre compte.';
+$_['text_thanks'] = 'Merci, ';
