fb0c26d806ab192064705e1a08020153366b8f17
P. Mergey <gzm@gizmecano.net>
2019-04-16 14:15:28 +0200

    Add </admin/mail/affiliate_deny.php>

---
+++ admin/language/fr-FR/mail/affiliate_deny.php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_subject'] = '%s - La création de votre compte d’affilié a été refusée !';
+$_['text_welcome'] = 'Bienvenue et merci pour votre enregistrement à %s !';
+$_['text_denied'] = 'Malheureusement, votre requête a été refusée. Pour plus d’information, vous pouvez contacter le propriétaire de la boutique ici :';
+$_['text_thanks'] = 'Merci, ';
