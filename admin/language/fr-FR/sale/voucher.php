a90b12a7b4a534ce0178f9a1f44beabcc942cfb9
P. Mergey <gzm@gizmecano.net>
2019-04-16 15:16:21 +0200

    Add </admin/sale/voucher.php>

---
+++ admin/language/fr-FR/sale/voucher.php
@@ -0,0 +1,56 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Bons d’achat';
+// Text
+$_['text_success'] = 'Les bons d’achat ont été modifiés avec succès !';
+$_['text_list'] = 'Liste des bons d’achat';
+$_['text_add'] = 'Ajouter un bon d’achat';
+$_['text_edit'] = 'Modifier un bon d’achat';
+$_['text_sent'] = 'Le bon d’achat a été envoyé avec succès !';
+// Column
+$_['column_name'] = 'Nom du bon d’achat';
+$_['column_code'] = 'Code';
+$_['column_from'] = 'De';
+$_['column_to'] = 'À';
+$_['column_theme'] = 'Thème';
+$_['column_amount'] = 'Montant';
+$_['column_status'] = 'Statut';
+$_['column_order_id'] = 'Commande n°';
+$_['column_customer'] = 'Client';
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_code'] = 'Code';
+$_['entry_from_name'] = 'Nom de l’expéditeur';
+$_['entry_from_email'] = 'Adresse électronique de l’expéditeur';
+$_['entry_to_name'] = 'Nom du destinataire';
+$_['entry_to_email'] = 'Adresse électronique du destinataire';
+$_['entry_theme'] = 'Thème';
+$_['entry_message'] = 'Message';
+$_['entry_amount'] = 'Montant';
+$_['entry_status'] = 'Statut';
+// Help
+$_['help_code'] = 'Le code que le client doit saisir pour activer le bon d’achat.';
+// Error
+$_['error_selection'] = 'Attention : aucun bon d’achat n’a été sélectionné !';
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les bons d’achat !';
+$_['error_exists'] = 'Attention : ce code de bon d’achat est déjà utilisé !';
+$_['error_code'] = 'Le code doit contenir entre 3 et 10 caractères !';
+$_['error_to_name'] = 'Le nom du destinataire doit contenir entre 1 et 64 caractères';
+$_['error_from_name'] = 'Le nom de l’expéditeur doit contenir entre 1 et 64 caractères';
+$_['error_email'] = 'L’adresse électronique semble être invalide !';
+$_['error_amount'] = 'Le montant doit être égal ou supérieur à 1 !';
+$_['error_order'] = 'Attention : ce bon d’achat ne peut pas être supprimé car il fait partie d’une <a href="%s">commande</a> !';
