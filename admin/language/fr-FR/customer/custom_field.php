c3082166de2ac6af932a1f335b25efaa036f2f7b
P. Mergey <gzm@gizmecano.net>
2019-04-16 14:35:49 +0200

    Add </admin/customer/custom_field.php>

---
+++ admin/language/fr-FR/customer/custom_field.php
@@ -0,0 +1,63 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Champs personnalisés';
+// Text
+$_['text_success'] = 'Les champs personnalisés ont été modifiés avec succès !';
+$_['text_list'] = 'Liste des champs personnalisés';
+$_['text_add'] = 'Ajouter un champ personnalisé';
+$_['text_edit'] = 'Modifier le champ personnalisé';
+$_['text_choose'] = 'Choisir';
+$_['text_select'] = 'Sélection';
+$_['text_radio'] = 'Radio';
+$_['text_checkbox'] = 'Case à cocher';
+$_['text_input'] = 'Entrée de données';
+$_['text_text'] = 'Texte';
+$_['text_textarea'] = 'Zone de texte';
+$_['text_file'] = 'Fichier';
+$_['text_date'] = 'Date';
+$_['text_datetime'] = 'Date et heure';
+$_['text_time'] = 'Heure';
+$_['text_account'] = 'Compte';
+$_['text_address'] = 'Adresse';
+$_['text_affiliate'] = 'Affilié';
+$_['text_regex'] = 'Expression régulière (Regex)';
+$_['text_custom_field'] = 'Champ personnalisé';
+$_['text_value'] = 'Valeurs de champ personnalisé';
+// Column
+$_['column_name'] = 'Nom du champ personnalisé';
+$_['column_location'] = 'Emplacement';
+$_['column_type'] = 'Type';
+$_['column_sort_order'] = 'Classement';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom du champ personnalisé';
+$_['entry_location'] = 'Emplacement';
+$_['entry_type'] = 'Type';
+$_['entry_value'] = 'Valeur';
+$_['entry_validation'] = 'Validation';
+$_['entry_custom_value'] = 'Nom de valeur du champ personnalisé';
+$_['entry_customer_group'] = 'Groupe de clients';
+$_['entry_required'] = 'Champ obligatoire';
+$_['entry_status'] = 'Statut';
+$_['entry_sort_order'] = 'Classement';
+// Help
+$_['help_regex'] = 'Utiliser les expressions régulières (Regex), par exemple : [a-zA-Z0-9_-]';
+$_['help_sort_order'] = 'Utiliser le signe moins pour revenir au champ précédent.';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les champs personnalisés !';
+$_['error_name'] = 'Le nom du champ personnalisé doit contenir entre 1 et 128 caractères !';
+$_['error_type'] = 'Attention : les valeurs du champ personnalisé sont requises !';
+$_['error_custom_value'] = 'Le nom du champ personnalisé doit contenir entre 1 et 128 caractères !';
