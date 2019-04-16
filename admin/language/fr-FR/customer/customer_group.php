2c7ea9706ede048ab3cc6a8672376a746aec7637
P. Mergey <gzm@gizmecano.net>
2019-04-16 14:32:09 +0200

    Add </admin/customer/customer_group.php>

---
+++ admin/language/fr-FR/customer/customer_group.php
@@ -0,0 +1,38 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Groupe de clients';
+// Text
+$_['text_success'] = 'Les groupes de clients ont été modifiés avec succès !';
+$_['text_list'] = 'Liste des groupes de clients';
+$_['text_add'] = 'Ajouter un groupe de clients';
+$_['text_edit'] = 'Modifier un groupe de clients';
+// Column
+$_['column_name'] = 'Nom du groupe de clients';
+$_['column_sort_order'] = 'Classement';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom du groupe de clients';
+$_['entry_description'] = 'Description';
+$_['entry_approval'] = 'Approuver de nouveaux clients';
+$_['entry_sort_order'] = 'Classement';
+// Help
+$_['help_approval'] = 'Les clients doivent avoir été approuvés par un administrateur avant de pouvoir se connecter.';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les groupes de clients !';
+$_['error_name'] = 'Un nom de groupe de clients doit contenir entre 3 et 32 caractères !';
+$_['error_default'] = 'Attention : ce groupe de clients ne peut pas être supprimé car il est actuellement assigné en tant que groupe de clients par défaut !';
+$_['error_store'] = 'Attention : ce groupe de clients ne peut pas être supprimé car il est actuellement attribué à %s boutiques !';
+$_['error_customer'] = 'Attention : ce groupe de clients ne peut pas être supprimé car il est actuellement attribué à %s clients !';
