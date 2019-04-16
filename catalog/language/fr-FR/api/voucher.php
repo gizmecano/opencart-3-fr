361620dadfd0cf854975a76ce0d947a0dc1f749f
P. Mergey <gzm@gizmecano.net>
2019-04-16 15:40:18 +0200

    Add </catalog/api/voucher.php>

---
+++ catalog/language/fr-FR/api/voucher.php
@@ -0,0 +1,26 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_success'] = 'La déduction de votre bon d’achat a été appliquée avec succès !';
+$_['text_cart'] = 'Votre panier d’achat a été modifié avec succès !';
+$_['text_for'] = '%s bon d’achat pour %s';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation d’accéder à l’API !';
+$_['error_voucher'] = 'Attention : le bon d’achat est invalide ou son solde a déjà été compensé !';
+$_['error_to_name'] = 'Le nom du destinataire doit contenir entre 1 et 64 caractères';
+$_['error_from_name'] = 'Votre nom doit contenir entre 1 et 64 caractères';
+$_['error_email'] = 'L’adresse électronique semble être invalide !';
+$_['error_theme'] = 'Vous devez sélectionner un thème !';
+$_['error_amount'] = 'le montant doit être compris entre %s et %s !';
