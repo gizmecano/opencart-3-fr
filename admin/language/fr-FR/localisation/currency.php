aaa134ecc72bb45a1a9740bb22abaef340d7a850
P. Mergey <gzm@gizmecano.net>
2019-04-17 07:50:19 +0200

    Add </admin/localisation/currency.php>

---
+++ admin/language/fr-FR/localisation/currency.php
@@ -0,0 +1,46 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Devises';
+// Text
+$_['text_success'] = 'Les devises ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des devises';
+$_['text_add'] = 'Ajouter une devise';
+$_['text_edit'] = 'Modifier une devise';
+$_['text_iso'] = 'Vous pouvez trouver une liste complète des codes ISO et des paramètres de devise sur <a href="http://www.xe.com/iso4217.php" target="_blank">cette page</a>.';
+// Column
+$_['column_title'] = 'Nom de la devise';
+$_['column_code'] = 'Code de la devise';
+$_['column_value'] = 'Valeur';
+$_['column_date_modified'] = 'Dernière mise à jour';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_title'] = 'Nom de la devise';
+$_['entry_code'] = 'Code de la devise';
+$_['entry_value'] = 'Valeur';
+$_['entry_symbol_left'] = 'Symbole de gauche';
+$_['entry_symbol_right'] = 'Symbole de droite';
+$_['entry_decimal_place'] = 'Emplacement des décimales';
+$_['entry_status'] = 'Statut';
+// Help
+$_['help_code'] = 'Ne changez pas le code s’il s’agit de la devise par défaut.';
+$_['help_value'] = 'Ajustez cette valeur à 1.00000 s’il s’agit de la devise par défaut.';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les devises !';
+$_['error_title'] = 'Le nom de la devise doit contenir entre 3 et 32 caractères !';
+$_['error_code'] = 'Le code de la devise doit contenir 3 caractères !';
+$_['error_default'] = 'Attention : cette devise ne peut pas être supprimée car elle est actuellement définie comme la devise par défaut !';
+$_['error_store'] = 'Attention : cette devise ne peut pas être supprimée car elle est actuellement attribuée à %s boutiques !';
+$_['error_order'] = 'Attention : cette devise ne peut pas être supprimée car elle est actuellement attribuée à %s commandes !';
