b2f3c44d7c92f2edad0727f9ccace6324a7cea51
P. Mergey <gzm@gizmecano.net>
2019-04-17 08:14:00 +0200

    Add </admin/localisation/language.php>

---
+++ admin/language/fr-FR/localisation/language.php
@@ -0,0 +1,45 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Langues';
+// Text
+$_['text_success'] = 'Les langues ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des langues';
+$_['text_add'] = 'Ajouter une langue';
+$_['text_edit'] = 'Modifier une langue';
+// Column
+$_['column_name'] = 'Nom de la langue';
+$_['column_code'] = 'Code de la langue';
+$_['column_sort_order'] = 'Classement';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom de la langue';
+$_['entry_code'] = 'Code de la langue';
+$_['entry_locale'] = 'Paramètres régionaux';
+$_['entry_status'] = 'Statut';
+$_['entry_sort_order'] = 'Classement';
+// Help
+$_['help_locale'] = 'Par exemple : en_US.UTF-8,en_US,en-gb,en_gb,english';
+$_['help_status'] = 'Masquer/Afficher dans le menu déroulant des langues';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les langues !';
+$_['error_exists'] = 'Attention : vous avez déjà ajouté cette langue !';
+$_['error_name'] = 'Le nom de la langue doit contenir entre 3 et 32 caractères !';
+$_['error_code'] = 'Le code de la langue doit contenir au moins 2 caractères !';
+$_['error_locale'] = 'Les paramètres régionaux sont requis !';
+$_['error_default'] = 'Attention : cette langue ne peut pas être supprimée car elle est actuellement assignée comme langue par défaut !';
+$_['error_admin'] = 'Attention : cette langue ne peut pas être supprimée car elle est actuellement assignée comme langue du panneau d’administration !';
+$_['error_store'] = 'Attention : cette langue ne peut pas être supprimée car elle est actuellement attribuée à %s boutiques !';
+$_['error_order'] = 'Attention : cette langue ne peut pas être supprimée car elle est actuellement attribuée à %s commandes !';
