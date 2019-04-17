6f8be18f82d146ce92a53d89cfafe8ad66eee8d3
P. Mergey <gzm@gizmecano.net>
2019-04-17 07:40:02 +0200

    Add </admin/sale/recurring.php>

---
+++ admin/language/fr-FR/sale/recurring.php
@@ -0,0 +1,90 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Commandes récurrentes';
+// Text
+$_['text_success'] = 'Les commandes récurrentes ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des commandes récurrentes';
+$_['text_filter'] = 'Filtre';
+$_['text_recurring_detail'] = 'Détails de commande récurrente';
+$_['text_order_detail'] = 'Détails de la commande';
+$_['text_product_detail'] = 'Détails de produit';
+$_['text_transaction'] = 'Transactions';
+$_['text_order_recurring_id'] = 'Identifiant de commande récurrente';
+$_['text_reference'] = 'Référence de paiement';
+$_['text_recurring_name'] = 'Profil de la commande récurrente';
+$_['text_recurring_description'] = 'Description de la commande récurrente';
+$_['text_recurring_status'] = 'Statut de la commande récurrente';
+$_['text_payment_method'] = 'Moyen de paiement';
+$_['text_order_id'] = 'Commande n°';
+$_['text_customer'] = 'Client';
+$_['text_email'] = 'Adresse électronique';
+$_['text_date_added'] = 'Date d’ajout';
+$_['text_order_status'] = 'Statut de la commande';
+$_['text_type'] = 'Type';
+$_['text_action'] = 'Action';
+$_['text_product'] = 'Produit';
+$_['text_quantity'] = 'Quantité';
+$_['text_amount'] = 'Montant';
+$_['text_cancel_payment'] = 'Annulation de paiement';
+$_['text_status_1'] = 'Activée';
+$_['text_status_2'] = 'Inactif';
+$_['text_status_3'] = 'Annulé';
+$_['text_status_4'] = 'Suspendu';
+$_['text_status_5'] = 'Expiré';
+$_['text_status_6'] = 'En attente';
+$_['text_transactions'] = 'Transactions';
+$_['text_cancel_confirm'] = 'L’annulation de commande récurrente ne peut pas être annulée ! Confirmez-vous vouloir le faire ?';
+$_['text_transaction_date_added'] = 'Date d’ajout';
+$_['text_transaction_payment'] = 'Paiement';
+$_['text_transaction_outstanding_payment'] = 'Paiement en suspens';
+$_['text_transaction_skipped'] = 'Paiement omis';
+$_['text_transaction_failed'] = 'Échec du paiement';
+$_['text_transaction_cancelled'] = 'Annulé';
+$_['text_transaction_suspended'] = 'Suspendu';
+$_['text_transaction_suspended_failed'] = 'Suspendu en raison d’un échec du paiement';
+$_['text_transaction_outstanding_failed'] = 'Paiement en suspens échoué';
+$_['text_transaction_expired'] = 'Expiré';
+$_['text_cancelled'] = 'Le paiement récurrent a été annulé';
+// Column
+$_['column_order_recurring_id'] = 'Identifiant de commande récurrente';
+$_['column_order_id'] = 'Commande n°';
+$_['column_reference'] = 'Référence de paiement';
+$_['column_customer'] = 'Client';
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_status'] = 'Statut';
+$_['column_amount'] = 'Montant';
+$_['column_type'] = 'Type';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_order_recurring_id'] = 'Identifiant de commande récurrente';
+$_['entry_order_id'] = 'Commande n°';
+$_['entry_reference'] = 'Référencement de paiement';
+$_['entry_customer'] = 'Client';
+$_['entry_date_added'] = 'Date d’ajout';
+$_['entry_status'] = 'Statut';
+$_['entry_type'] = 'Type';
+$_['entry_action'] = 'Action';
+$_['entry_email'] = 'Adresse électronique';
+$_['entry_description'] = 'Description du profil récurrent';
+$_['entry_product'] = 'Produit';
+$_['entry_quantity'] = 'Quantité';
+$_['entry_amount'] = 'Montant';
+$_['entry_recurring'] = 'Profil récurrent';
+$_['entry_payment_method'] = 'Moyen de paiement';
+$_['entry_cancel_payment'] = 'Annuler le paiement';
+// Error
+$_['error_not_cancelled'] = 'Erreur : %s';
+$_['error_not_found'] = 'Le profil récurrent ne peut pas être annulé';
