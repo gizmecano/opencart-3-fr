07316174bb43e422fc1f7bb80720326a8f0a72d3
P. Mergey <gzm@gizmecano.net>
2019-04-17 08:26:48 +0200

    Add </admin/localisation/tax_rate.php>

---
+++ admin/language/fr-FR/localisation/tax_rate.php
@@ -0,0 +1,42 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Taux de taxe';
+// Text
+$_['text_success'] = 'Les taux de taxe ont été modifiés avec succès !';
+$_['text_list'] = 'Liste des taux de taxe';
+$_['text_add'] = 'Ajouter un taux de taxe';
+$_['text_edit'] = 'Modifier un taux de taxe';
+$_['text_percent'] = 'Pourcentage';
+$_['text_amount'] = 'Montant fixe';
+// Column
+$_['column_name'] = 'Nom du taux';
+$_['column_rate'] = 'Taux de taxe';
+$_['column_type'] = 'Type';
+$_['column_geo_zone'] = 'Zone géographique';
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_date_modified'] = 'Date de modification';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom du taux';
+$_['entry_rate'] = 'Taux de taxe';
+$_['entry_type'] = 'Type';
+$_['entry_customer_group'] = 'Groupe de clients';
+$_['entry_geo_zone'] = 'Zone géographique';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les taux de taxe !';
+$_['error_tax_rule'] = 'Attention : ce taux de taxe ne peut pas être supprimé car il est actuellement attribué à %s classes de taxes !';
+$_['error_name'] = 'Le nom du taux doit contenir entre 1 et 32 caractères !';
+$_['error_rate'] = 'Le taux de taxe est requis !';
