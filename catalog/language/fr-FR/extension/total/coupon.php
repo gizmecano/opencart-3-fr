b31a92af87e048901907b133afbb6aa1ffc7d2d7
P. Mergey <gzm@gizmecano.net>
2019-04-17 09:24:23 +0200

    Add </catalog/extension/total/coupon.php>

---
+++ catalog/language/fr-FR/extension/total/coupon.php
@@ -0,0 +1,24 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Utiliser un bon de réduction';
+// Text
+$_['text_coupon'] = 'Bon de réduction (%s)';
+$_['text_success'] = 'La déduction de votre bon a été appliquée';
+// Entry
+$_['entry_coupon'] = 'Saisissez le code du bon de réduction ici';
+// Error
+$_['error_coupon'] = 'Attention : le bon est invalide, a expiré ou a déjà été utilisé !';
+$_['error_empty'] = 'Attention : veuillez saisir le code du bon de réduction !';
