9e3e849f78305332daa30ca60633442241200010
P. Mergey <gzm@gizmecano.net>
2019-04-17 09:34:14 +0200

    Add </catalog/api/shipping.php>

---
+++ catalog/language/fr-FR/api/shipping.php
@@ -0,0 +1,30 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_address'] = 'L’adresse de livraison a été configurée avec succès !';
+$_['text_method'] = 'Le mode de livraison a été configuré avec succès !';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation d’accéder à l’API !';
+$_['error_firstname'] = 'Le prénom doit contenir entre 1 à 32 caractères !';
+$_['error_lastname'] = 'Le nom de famille doit contenir entre 1 à 32 caractères !';
+$_['error_address_1'] = 'L’adresse doit contenir entre 3 et 128 caractères !';
+$_['error_city'] = 'Attention : le nom de la localité doit contenir entre 3 et 128 caractères !';
+$_['error_postcode'] = 'Attention : le code postal doit contenir entre 2 à 10 caractères !';
+$_['error_country'] = 'Veuillez sélectionner un pays !';
+$_['error_zone'] = 'Veuillez sélectionner une subdivision administrative !';
+$_['error_custom_field'] = '%s est requis !';
+$_['error_address'] = 'Attention : une adresse de livraison est requise !';
+$_['error_method'] = 'Attention : un mode de livraison est requis !';
+$_['error_no_shipping'] = 'Attention : aucune option de livraison n’est disponible !';
