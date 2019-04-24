f450ded937f8c4afa4262bb306f84a0b77ab6400
P. Mergey <gzm@gizmecano.net>
2019-04-24 10:28:14 +0200

    Add </catalog/mail/order_edit.php>

---
+++ catalog/language/fr-FR/mail/order_edit.php
@@ -0,0 +1,22 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_subject'] = '%s - Commande %s';
+$_['text_order_id'] = 'Commande n° :';
+$_['text_date_added'] = 'Date d’ajout : ';
+$_['text_order_status'] = 'Votre commande a été modifiée selon le statut suivant : ';
+$_['text_comment'] = 'Les commentaires pour votre commande sont : ';
+$_['text_link'] = 'Pour consulter votre commande, cliquez sur le lien ci-dessous : ';
+$_['text_footer'] = 'Vous pouvez répondre à ce courrier dans le cas où vous auriez une question.';
