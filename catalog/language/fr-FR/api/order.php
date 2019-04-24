20e4153e334f8b8adef8c4f1197315c7cf996c39
P. Mergey <gzm@gizmecano.net>
2019-04-24 10:20:10 +0200

    Add </catalog/api/order.php>

---
+++ catalog/language/fr-FR/api/order.php
@@ -0,0 +1,28 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_success'] = 'Votre commande a été modifiée avec succès';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation d’accéder à l’API !';
+$_['error_customer'] = 'Attention : les détails de l’utilisateur doivent avoir été configurés';
+$_['error_payment_address'] = 'Attention : une adresse de paiement est requise';
+$_['error_payment_method'] = 'Attention : un moyen de paiement est requis !';
+$_['error_no_payment'] = 'Attention : aucune option de paiement n’est disponible !';
+$_['error_shipping_address'] = 'Attention : une adresse de livraison est requise !';
+$_['error_shipping_method'] = 'Attention : un mode de livraison est requis !';
+$_['error_no_shipping'] = 'Attention : aucune option d’expédition n’est disponible !';
+$_['error_stock'] = 'Attention : les produits indiqués avec *** ne sont pas disponibles dans la quantité désirée ou ne sont actuellement pas en stock !';
+$_['error_minimum'] = 'Attention : le montant minimum d’une commande pour %s est %s !';
+$_['error_not_found'] = 'Attention : la commande est introuvable !';
