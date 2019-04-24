f6f86b518a6837ae8a4182c725b64bbf47720294
P. Mergey <gzm@gizmecano.net>
2019-04-24 11:22:05 +0200

    Add </catalog/account/transaction.php>

---
+++ catalog/language/fr-FR/account/transaction.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Vos transactions';
+// Column
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_description'] = 'Description';
+$_['column_amount'] = 'Montant (%s)';
+// Text
+$_['text_account'] = 'Compte';
+$_['text_transaction'] = 'Vos transactions';
+$_['text_total'] = 'La balance de votre compte est actuellement : ';
+$_['text_empty'] = 'Vous n’avez aucune transaction !';
