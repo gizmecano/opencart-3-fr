647ec1d80a057dbf6c2d7e2e05162f344a0f014a
P. Mergey <gzm@gizmecano.net>
2019-04-24 10:16:48 +0200

    Add </catalog/account/order.php>

---
+++ catalog/language/fr-FR/account/order.php
@@ -0,0 +1,47 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Historique des commandes';
+// Text
+$_['text_account'] = 'Compte';
+$_['text_order'] = 'Informations sur la commande';
+$_['text_order_detail'] = 'Détails de la commande';
+$_['text_invoice_no'] = 'Facture n° : ';
+$_['text_order_id'] = 'Identifiant de la commande : ';
+$_['text_date_added'] = 'Date d’ajout : ';
+$_['text_shipping_address'] = 'Adresse de livraison';
+$_['text_shipping_method'] = 'Mode de livraison : ';
+$_['text_payment_address'] = 'Adresse de facturation';
+$_['text_payment_method'] = 'Moyen de paiement : ';
+$_['text_comment'] = 'Commentaire';
+$_['text_history'] = 'Historique des commandes';
+$_['text_success'] = 'Vous avez ajouté <a href="%s">%s</a> à votre <a href="%s">panier d’achat</a> avec succès !';
+$_['text_empty'] = 'Vous n’avez effectué aucune commande précédemment';
+$_['text_error'] = 'La commande que vous avez demandée est introuvable !';
+// Column
+$_['column_order_id'] = 'Commande n°';
+$_['column_customer'] = 'Client';
+$_['column_product'] = 'Nombre de produits';
+$_['column_name'] = 'Nom du produit';
+$_['column_model'] = 'Référence';
+$_['column_quantity'] = 'Quantité';
+$_['column_price'] = 'Prix';
+$_['column_total'] = 'Total';
+$_['column_action'] = 'Action';
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_status'] = 'Statut';
+$_['column_comment'] = 'Commentaire';
+// Error
+$_['error_reorder'] = '%s n’est pas disponible pour être réorganisées actuellement.';
