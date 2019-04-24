2fac752afa8fcdf6603a7981525aedfbbb9208dc
P. Mergey <gzm@gizmecano.net>
2019-04-24 10:12:21 +0200

    Add </admin/sale/order.php>

---
+++ admin/language/fr-FR/sale/order.php
@@ -0,0 +1,140 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Commandes';
+// Text
+$_['text_success'] = 'Les commandes ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des commandes';
+$_['text_add'] = 'Ajouter une commande';
+$_['text_edit'] = 'Modifier une commande';
+$_['text_filter'] = 'Filtre';
+$_['text_order_detail'] = 'Détails de la commande';
+$_['text_customer_detail'] = 'Détails du client';
+$_['text_option'] = 'Options';
+$_['text_store'] = 'Boutique';
+$_['text_date_added'] = 'Date d’ajout';
+$_['text_payment_method'] = 'Moyen de paiement';
+$_['text_shipping_method'] = 'Mode de livraison';
+$_['text_customer'] = 'Client';
+$_['text_customer_group'] = 'Groupe de clients';
+$_['text_email'] = 'Adresse électronique';
+$_['text_telephone'] = 'Téléphone';
+$_['text_fax'] = 'Télécopie';
+$_['text_invoice'] = 'Facture';
+$_['text_reward'] = 'Points de fidélité';
+$_['text_affiliate'] = 'Affilié';
+$_['text_order'] = 'Commande (#%s)';
+$_['text_payment_address'] = 'Adresse de facturation';
+$_['text_shipping_address'] = 'Adresse de livraison';
+$_['text_comment'] = 'Commentaire';
+$_['text_history'] = 'Historique des commandes';
+$_['text_history_add'] = 'Ajouter à l’historique des commandes';
+$_['text_account_custom_field'] = 'Champs personnalisés du compte';
+$_['text_payment_custom_field'] = 'Champs personnalisés du moyen de paiement';
+$_['text_shipping_custom_field'] = 'Champs personnalisés du mode de livraison';
+$_['text_browser'] = 'Navigateur';
+$_['text_ip'] = 'Adresse IP';
+$_['text_forwarded_ip'] = 'Adresse IP suivie';
+$_['text_user_agent'] = 'Agent utilisateur';
+$_['text_accept_language'] = 'Langue acceptée';
+$_['text_order_id'] = 'Commande n° : ';
+$_['text_website'] = 'Site internet : ';
+$_['text_invoice_no'] = 'Facture n° : ';
+$_['text_invoice_date'] = 'Date de facturation : ';
+$_['text_sku'] = 'SKU : ';
+$_['text_upc'] = 'UPC : ';
+$_['text_ean'] = 'EAN : ';
+$_['text_jan'] = 'JAN : ';
+$_['text_isbn'] = 'ISBN : ';
+$_['text_mpn'] = 'MPN : ';
+$_['text_missing'] = 'Commandes manquantes';
+$_['text_default'] = 'Par défaut';
+$_['text_product'] = 'Ajouter produit(s)';
+$_['text_voucher'] = 'Ajouter bon(s) d’achat';
+$_['text_shipping'] = 'Livraison';
+$_['text_contact'] = 'Contact';
+$_['text_reward_added'] = 'Les points de fidélité ont été ajoutés avec succès !';
+$_['text_reward_removed'] = 'Les points de fidélité ont été retirés avec succès !';
+$_['text_commission_added'] = 'La commission a été ajoutée avec succès !';
+$_['text_commission_removed'] = 'La commission a été retirée avec succès !';
+$_['text_restock'] = 'Les produits ont été réapprovisionnés avec succès !';
+$_['text_upload'] = 'Le transfert du fichier a réussi !';
+$_['text_picklist'] = 'Bordereau d’expédition';
+// Column
+$_['column_order_id'] = 'Commande n°';
+$_['column_customer'] = 'Client';
+$_['column_status'] = 'Statut';
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_date_modified'] = 'Date de modification';
+$_['column_total'] = 'Total';
+$_['column_product'] = 'Produit';
+$_['column_model'] = 'Référence';
+$_['column_quantity'] = 'Quantité';
+$_['column_price'] = 'Prix unitaire';
+$_['column_comment'] = 'Commentaire';
+$_['column_notify'] = 'Client informé';
+$_['column_location'] = 'Emplacement';
+$_['column_reference'] = 'Référence';
+$_['column_action'] = 'Action';
+$_['column_weight'] = 'Poids du produit';
+// Entry
+$_['entry_store'] = 'Boutique';
+$_['entry_customer'] = 'Client';
+$_['entry_customer_group'] = 'Groupe de clients';
+$_['entry_firstname'] = 'Prénom';
+$_['entry_lastname'] = 'Nom de famille';
+$_['entry_email'] = 'Adresse électronique';
+$_['entry_telephone'] = 'Téléphone';
+$_['entry_address'] = 'Choisir l’adresse';
+$_['entry_company'] = 'Société';
+$_['entry_address_1'] = 'Adresse 1';
+$_['entry_address_2'] = 'Adresse 2';
+$_['entry_city'] = 'Localité';
+$_['entry_postcode'] = 'Code postal';
+$_['entry_country'] = 'Pays';
+$_['entry_zone'] = 'Subdivision administrative';
+$_['entry_zone_code'] = 'Code de la subdivision administrative';
+$_['entry_product'] = 'Choisir un produit';
+$_['entry_option'] = 'Choisir une (ou des) options(s)';
+$_['entry_quantity'] = 'Quantité';
+$_['entry_to_name'] = 'Nom du destinataire';
+$_['entry_to_email'] = 'Adresse électronique du destinataire';
+$_['entry_from_name'] = 'Nom de l’expéditeur';
+$_['entry_from_email'] = 'Adresse électronique de l’expéditeur';
+$_['entry_theme'] = 'Thème du bon d’achat';
+$_['entry_message'] = 'Message';
+$_['entry_amount'] = 'Montant';
+$_['entry_affiliate'] = 'Affilié';
+$_['entry_order_status'] = 'Statut de la commande';
+$_['entry_notify'] = 'Informer le client';
+$_['entry_override'] = 'Outrepasser';
+$_['entry_comment'] = 'Commentaire';
+$_['entry_currency'] = 'Devise';
+$_['entry_shipping_method'] = 'Mode de livraison';
+$_['entry_payment_method'] = 'Moyen de paiement';
+$_['entry_coupon'] = 'Bon de réduction';
+$_['entry_voucher'] = 'Bon d’achat';
+$_['entry_reward'] = 'Points de fidélité';
+$_['entry_order_id'] = 'Commande n°';
+$_['entry_total'] = 'Total';
+$_['entry_date_added'] = 'Date d’ajout';
+$_['entry_date_modified'] = 'Date de modification';
+// Help
+$_['help_override'] = 'Si la commande d’un client est bloquée pour une modification du statut de commande en raison d’une extension anti-fraude, vous pouvez activer l’outrepassement.';
+// Error
+$_['error_warning'] = 'Attention : veuillez vérifier attentivement les erreurs dans le formulaire !';
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les commandes !';
+$_['error_action'] = 'Attention : cette action ne peut pas être terminée !';
+$_['error_filetype'] = 'Attention : le type de fichier est incorrect !';
