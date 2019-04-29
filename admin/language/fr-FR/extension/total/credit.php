37eb77a26e11ee41429445aa79f757eb87f73096
P. Mergey <gzm@gizmecano.net>
2019-04-29 10:06:00 +0200

    Add </admin/extension/total/credit.php>

---
+++ admin/language/fr-FR/extension/total/credit.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Crédit de la boutique';
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Le total du crédit de la boutique a été modifié avec succès !';
+$_['text_edit'] = 'Modifier le total du crédit de la boutique';
+// Entry
+$_['entry_status'] = 'Statut';
+$_['entry_sort_order'] = 'Classement';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier le total du crédit de la boutique !';
