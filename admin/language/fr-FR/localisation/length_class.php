97d6a2a91f21e12476dddd17475d25b2d56deb6e
P. Mergey <gzm@gizmecano.net>
2019-04-29 08:15:04 +0200

    Add </admin/localisation/length_class.php>

---
+++ admin/language/fr-FR/localisation/length_class.php
@@ -0,0 +1,38 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Unités de longueur';
+// Text
+$_['text_success'] = 'Les unités de longueur ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des unités de longueur';
+$_['text_add'] = 'Ajouter une unité de longueur';
+$_['text_edit'] = 'Modifier une unité de longueur';
+// Column
+$_['column_title'] = 'Nom de l’unité de longueur';
+$_['column_unit'] = 'Unité de longueur';
+$_['column_value'] = 'Valeur';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_title'] = 'Nom de la longueur';
+$_['entry_unit'] = 'Unité de la longueur';
+$_['entry_value'] = 'Valeur';
+// Help
+$_['help_value'] = 'Ajustez à 1.00000 s’il s’agit de votre longueur par défaut.';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les unités de longueur !';
+$_['error_title'] = 'Le nom de l’unité de longueur doit contenir entre 3 et 32 caractères !';
+$_['error_unit'] = 'L’unité de longueur doit contenir entre 1 et 4 caractères !';
+$_['error_default'] = 'Attention : cette unité de longueur ne peut pas être supprimée car elle est actuellement assignée comme unité de longueur par défaut !';
+$_['error_product'] = 'Attention : cette unité de longueur ne peut pas être supprimée car elle est actuellement attribuée à %s produits !';
