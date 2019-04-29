26337119397aeb6f8525910f91be3006b7fcc4d5
P. Mergey <gzm@gizmecano.net>
2019-04-29 08:20:13 +0200

    Add </admin/localisation/weight_class.php>

---
+++ admin/language/fr-FR/localisation/weight_class.php
@@ -0,0 +1,38 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Unités de poids';
+// Text
+$_['text_success'] = 'Les unités de poids ont été modifiées avec succès !';
+$_['text_list'] = 'Liste des unités de poids';
+$_['text_add'] = 'Ajouter une unité de poids';
+$_['text_edit'] = 'Modifier une unité de poids';
+// Column
+$_['column_title'] = 'Nom de l’unité de poids';
+$_['column_unit'] = 'Unité de poids';
+$_['column_value'] = 'Valeur';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_title'] = 'Nom de l’unité de poids';
+$_['entry_unit'] = 'Unité de poids';
+$_['entry_value'] = 'Valeur';
+// Help
+$_['help_value'] = 'Ajustez à 1.00000 s’il s’agit de votre poids par défaut.';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier les unités de poids !';
+$_['error_title'] = 'Le nom de l’unité de poids doit contenir entre 3 et 32 caractères !';
+$_['error_unit'] = 'L’unité de poids doit contenir entre 1 et 4 caractères !';
+$_['error_default'] = 'Attention : cette unité de poids ne peut pas être supprimée car elle est actuellement assignée comme unité de poids par défaut !';
+$_['error_product'] = 'Attention : cette unité de poids ne peut pas être supprimée car elle est actuellement attribuée à %s produits !';
