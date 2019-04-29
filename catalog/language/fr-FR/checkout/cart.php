b64537279216fa85b54cc0ccb4d84447e7ebca91
P. Mergey <gzm@gizmecano.net>
2019-04-29 10:35:12 +0200

    Add </catalog/checkout/cart.php>

---
+++ catalog/language/fr-FR/checkout/cart.php
@@ -0,0 +1,51 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Panier d’achat';
+// Text
+$_['text_success'] = 'Vous avez ajouté <a href="%s">%s</a> à votre <a href="%s">panier d’achat</a> avec succès !';
+$_['text_remove'] = 'Votre panier d’achat a été modifié avec succès !';
+$_['text_login'] = 'Attention : vous devez <a href="%s">vous connecter</a> ou <a href="%s">créer un compte</a> pour consulter les prix !';
+$_['text_items'] = '%s articles(s) - %s';
+$_['text_points'] = 'Points de fidélité : %s';
+$_['text_next'] = 'Que souhaitez-vous faire ensuite ?';
+$_['text_next_choice'] = 'Choisissez si vous souhaitez utiliser un bon de réduction ou un bon d’achat pour estimer les frais d’expédition.';
+$_['text_empty'] = 'Votre panier d’achat est vide !';
+$_['text_day'] = 'jour';
+$_['text_week'] = 'semaine';
+$_['text_semi_month'] = 'demi-mois';
+$_['text_month'] = 'mois';
+$_['text_year'] = 'année';
+$_['text_trial'] = '%s chaque %s %s pour %s paiements soit ';
+$_['text_recurring'] = '%s chaque %s %s';
+$_['text_payment_cancel'] = 'jusqu’à annulation';
+$_['text_recurring_item'] = 'Élément récurrent';
+$_['text_payment_recurring'] = 'Profil de paiement';
+$_['text_trial_description'] = '%s chaque %d %s(s) pour %d paiement(s) soit';
+$_['text_payment_description'] = '%s chaque %d %s(s) pour %d paiement(s)';
+$_['text_payment_cancel'] = '%s chaque %d %s(s) jusqu’à annulation';
+// Column
+$_['column_image'] = 'Image';
+$_['column_name'] = 'Nom du produit';
+$_['column_model'] = 'Référence';
+$_['column_quantity'] = 'Quantité';
+$_['column_price'] = 'Prix unitaire';
+$_['column_total'] = 'Total';
+// Error
+$_['error_stock'] = 'Attention : les produits indiqués avec *** ne sont pas disponibles dans la quantité désirée ou ne sont actuellement pas en stock !';
+$_['error_minimum'] = 'Le montant minimum d’une commande pour %s est %s !';
+$_['error_required'] = '%s est requis !';
+$_['error_product'] = 'Attention : il n’y a aucun produit dans votre panier !';
+$_['error_recurring_required'] = 'Veuillez sélectionner un paiement récurrent !';
