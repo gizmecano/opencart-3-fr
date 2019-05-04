0546ef2611df059aa5fb723c3d018c4ef1af1fb0
P. Mergey <gzm@gizmecano.net>
2019-05-04 13:58:20 +0200

    Add </catalog/information/sitemap.php>

---
+++ catalog/language/fr-FR/information/sitemap.php
@@ -0,0 +1,29 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Plan du site';
+// Text
+$_['text_special'] = 'Promotions';
+$_['text_account'] = 'Mon compte';
+$_['text_edit'] = 'Informations du compte';
+$_['text_password'] = 'Mot de passe';
+$_['text_address'] = 'Carnet d’adresses';
+$_['text_history'] = 'Historique des commandes';
+$_['text_download'] = 'Téléchargements';
+$_['text_cart'] = 'Panier d’achat';
+$_['text_checkout'] = 'Valider la commande';
+$_['text_search'] = 'Rechercher';
+$_['text_information'] = 'Informations';
+$_['text_contact'] = 'Contact';
