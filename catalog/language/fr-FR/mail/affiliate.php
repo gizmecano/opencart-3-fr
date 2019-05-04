a028033a88f7af26ecf5a68ea5409a695dabebb9
P. Mergey <gzm@gizmecano.net>
2019-05-04 13:38:41 +0200

    Add </catalog/mail/affiliate.php>

---
+++ catalog/language/fr-FR/mail/affiliate.php
@@ -0,0 +1,30 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_subject'] = '%s - Programme d’affiliation';
+$_['text_welcome'] = 'Merci de rejoindre le programme d’affiliation %s !';
+$_['text_login'] = 'Votre compte a été créé. Vous pouvez vous connecter en utilisant votre adresse électronique et votre mot de passe sur notre site internet ou à l’adresse suivante : ';
+$_['text_approval'] = 'Votre compte doit être approuvé avant que vous puissiez vous connecter. Une fois approuvé, vous pourrez vous connecter en utilisant votre adresse électronique et le mot de passe sur notre site internet ou à l’adresse suivante : ';
+$_['text_services'] = 'Une fois votre connexion établie, vous serez en mesure de générer des codes de suivi, suivre le paiement des commissions et modifier les informations de votre compte.';
+$_['text_thanks'] = 'Merci,';
+$_['text_new_affiliate'] = 'Nouvel affilié';
+$_['text_signup'] = 'Un nouvel affilié a souscrit : ';
+$_['text_website'] = 'Site internet :';
+$_['text_customer_group'] = 'Groupe de client :';
+$_['text_firstname'] = 'Prénom : ';
+$_['text_lastname'] = 'Nom de famille : ';
+$_['text_company'] = 'Société : ';
+$_['text_email'] = 'Adresse électronique : ';
+$_['text_telephone'] = 'Numéro de téléphone : ';
