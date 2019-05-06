dd369c0d7f7a6b47dc9169a2547b73b21461b7c5
P. Mergey <gzm@gizmecano.net>
2019-05-06 10:26:27 +0200

    Add </admin/extension/report/sale_coupon.php>

---
+++ admin/language/fr-FR/extension/report/sale_coupon.php
@@ -0,0 +1,34 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Rapport des bons de réduction';
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_edit'] = 'Modifier le rapport des bons de réduction';
+$_['text_success'] = 'Le rapport des bons de réduction a été modifié avec succès !';
+$_['text_filter'] = 'Filtre';
+// Column
+$_['column_name'] = 'Nom du bon de réduction';
+$_['column_code'] = 'Code du bon de réduction';
+$_['column_orders'] = 'Commandes';
+$_['column_total'] = 'Total';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_date_start'] = 'Date de début';
+$_['entry_date_end'] = 'Date de fin';
+$_['entry_status'] = 'Statut';
+$_['entry_sort_order'] = 'Classement';
+// Error
+$_['error_permission'] = 'Attention : vous n’avez pas l’autorisation de modifier le rapport des bons de réduction !';
