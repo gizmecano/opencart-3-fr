36537a817f8049bbac90533584d892e26f4d31d1
P. Mergey <gzm@gizmecano.net>
2019-05-06 10:48:35 +0200

    Add </catalog/product/product.php>

---
+++ catalog/language/fr-FR/product/product.php
@@ -0,0 +1,60 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_search'] = 'Rechercher';
+$_['text_brand'] = 'Marque du fabricant';
+$_['text_manufacturer'] = 'Fabricant : ';
+$_['text_model'] = 'Référence : ';
+$_['text_reward'] = 'Points de fidélité : ';
+$_['text_points'] = 'Prix en points de fidélité : ';
+$_['text_stock'] = 'Disponibilité : ';
+$_['text_instock'] = 'Disponible';
+$_['text_tax'] = 'Prix hors taxes : ';
+$_['text_discount'] = ' ou plus ';
+$_['text_option'] = 'Options disponibles';
+$_['text_minimum'] = 'Ce produit a une quantité minimale de %s';
+$_['text_reviews'] = '%s évaluation(s)';
+$_['text_write'] = 'Donnez votre avis';
+$_['text_login'] = 'Veuillez vous <a href="%s">connecter</a> ou vous <a href="%s">inscrire</a> pour évaluer';
+$_['text_no_reviews'] = 'Il n’y a aucune évaluation sur ce produit.';
+$_['text_note'] = '<span class="text-danger"> Attention :</span> le HTML n’est pas transcrit !';
+$_['text_success'] = 'Merci pour votre évaluation. Elle a été soumise au webmestre pour approbation.';
+$_['text_related'] = 'Produits liés';
+$_['text_tags'] = 'Mots-clefs : ';
+$_['text_error'] = 'Le produit est introuvable !';
+$_['text_payment_recurring'] = 'Profils de paiement';
+$_['text_trial_description'] = '%s chaque %s(s) %d pour %d paiements alors';
+$_['text_payment_description'] = '%s chaque %s(s) %d pour %d paiements';
+$_['text_payment_until_canceled_description'] = '%s chaque %s(s) %d jusqu’à annulée';
+$_['text_day'] = 'Jour';
+$_['text_week'] = 'Semaine';
+$_['text_semi_month'] = 'Demi-mois';
+$_['text_month'] = 'Mois';
+$_['text_year'] = 'Année';
+// Entry
+$_['entry_qty'] = 'Quantité';
+$_['entry_name'] = 'Votre nom';
+$_['entry_review'] = 'Votre évaluation';
+$_['entry_rating'] = 'Note';
+$_['entry_good'] = 'Bon';
+$_['entry_bad'] = 'Mauvais';
+// Tabs
+$_['tab_description'] = 'Description';
+$_['tab_attribute'] = 'Spécification';
+$_['tab_review'] = 'Évaluations (%s)';
+// Error
+$_['error_name'] = 'Attention : le nom de l’évaluation doit être compris entre 3 et 25 caractères !';
+$_['error_text'] = 'Attention : le texte de l’évaluation doit être compris entre 25 et 1000 caractères !';
+$_['error_rating'] = 'Attention : veuillez sélectionner une note !';
