9cd3a0e93f9a52a0ab9023aa00baa36881224f45
P. Mergey <gzm@gizmecano.net>
2019-05-06 11:00:11 +0200

    Add </admin/common/column_left.php>

---
+++ admin/language/fr-FR/common/column_left.php
@@ -0,0 +1,103 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_api'] = 'API';
+$_['text_attribute'] = 'Attributs';
+$_['text_attribute_group'] = 'Groupe d’attributs';
+$_['text_backup'] = 'Sauvegarde et restauration';
+$_['text_banner'] = 'Bannières';
+$_['text_catalog'] = 'Catalogue';
+$_['text_category'] = 'Catégories';
+$_['text_country'] = 'Pays';
+$_['text_coupon'] = 'Bons de réduction';
+$_['text_currency'] = 'Devises';
+$_['text_customer'] = 'Clients';
+$_['text_customer_group'] = 'Groupes de client';
+$_['text_customer_online'] = 'Visiteurs en ligne';
+$_['text_customer_approval'] = 'Approbations de clients';
+$_['text_custom_field'] = 'Champs personnalisés';
+$_['text_dashboard'] = 'Tableau de bord';
+$_['text_design'] = 'Mise en page';
+$_['text_download'] = 'Téléchargements';
+$_['text_log'] = 'Journaux d’erreurs';
+$_['text_event'] = 'Événements';
+$_['text_extension'] = 'Extensions';
+$_['text_filter'] = 'Filtres';
+$_['text_geo_zone'] = 'Zones géographiques';
+$_['text_information'] = 'Informations';
+$_['text_installer'] = 'Installateur d’extensions';
+$_['text_language'] = 'Langues';
+$_['text_language_editor'] = 'Éditeur linguistique';
+$_['text_layout'] = 'Gabarits';
+$_['text_localisation'] = 'Localisation';
+$_['text_location'] = 'Emplacement';
+$_['text_contact'] = 'Contact';
+$_['text_marketing'] = 'Marketing';
+$_['text_marketplace'] = 'Cybermarché';
+$_['text_maintenance'] = 'Maintenance';
+$_['text_menu'] = 'Gestionnaire de menu';
+$_['text_modification'] = 'Modifications';
+$_['text_manufacturer'] = 'Fabricants';
+$_['text_navigation'] = 'Navigation';
+$_['text_option'] = 'Options';
+$_['text_order'] = 'Commandes';
+$_['text_order_status'] = 'Statuts de commande';
+$_['text_online'] = 'Qui est en ligne';
+$_['text_product'] = 'Produits';
+$_['text_reports'] = 'Rapports';
+$_['text_review'] = 'Évaluations';
+$_['text_return'] = 'Retours';
+$_['text_return_action'] = 'Actions des retours';
+$_['text_return_reason'] = 'Motifs des retours';
+$_['text_return_status'] = 'Statuts des retours';
+$_['text_sale'] = 'Ventes';
+$_['text_setting'] = 'Paramètres';
+$_['text_seo_url'] = 'URL de référencement (SEO)';
+$_['text_statistics'] = 'Statistiques';
+$_['text_stock_status'] = 'Statuts des stocks';
+$_['text_system'] = 'Système';
+$_['text_tax'] = 'Taxes';
+$_['text_tax_class'] = 'Classes de taxes';
+$_['text_tax_rate'] = 'Taux de taxe';
+$_['text_translation'] = 'Éditeur linguistique';
+$_['text_theme'] = 'Éditeur de thème';
+$_['text_upload'] = 'Transferts de fichiers';
+$_['text_user'] = 'Utilisateur';
+$_['text_users'] = 'Utilisateurs';
+$_['text_user_group'] = 'Groupes d’utilisateurs';
+$_['text_voucher'] = 'Bons d’achat';
+$_['text_voucher_theme'] = 'Thèmes des bons d’achat';
+$_['text_weight_class'] = 'Unités de poids';
+$_['text_length_class'] = 'Unités de longueur';
+$_['text_zone'] = 'Domaines';
+$_['text_recurring'] = 'Profils récurrents';
+$_['text_order_recurring'] = 'Commandes récurrentes';
+$_['text_openbay_extension'] = 'OpenBay Pro';
+$_['text_openbay_dashboard'] = 'Tableau de bord OpenBay';
+$_['text_openbay_orders'] = 'Mise à jour des commandes automatisée OpenBay';
+$_['text_openbay_items'] = 'Gérer les produits';
+$_['text_openbay_ebay'] = 'eBay';
+$_['text_openbay_amazon'] = 'Amazon (EU)';
+$_['text_openbay_amazonus'] = 'Amazon (US)';
+$_['text_openbay_etsy'] = 'Etsy';
+$_['text_openbay_settings'] = 'Paramètres';
+$_['text_openbay_links'] = 'Liens du produit';
+$_['text_openbay_report_price'] = 'Rapport des prix';
+$_['text_openbay_order_import'] = 'Importer une commande';
+$_['text_paypal'] = 'PayPal';
+$_['text_paypal_search'] = 'Recherche de transactions';
+$_['text_complete_status'] = 'Commandes complètes';
+$_['text_processing_status'] = 'Commandes en cours';
+$_['text_other_status'] = 'Autres statuts';
