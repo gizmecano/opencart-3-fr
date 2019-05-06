96737b3bc0ee80c54aef404ad3d01e2f5647c98e
P. Mergey <gzm@gizmecano.net>
2019-05-06 11:04:29 +0200

    Add </admin/common/footer.php>

---
+++ admin/language/fr-FR/common/footer.php
@@ -0,0 +1,17 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Text
+$_['text_footer'] = '<a href="https://www.opencart.com/" target="_blank">OpenCart</a> © 2009-2017 Tous droits réservés<br/><a href="https://github.com/gizmecano/opencart-3-fr/" target="_blank">opencart-3-fr</a> © 2019 P. Mergey';
+$_['text_version'] = 'Version %s';
