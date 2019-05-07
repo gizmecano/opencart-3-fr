b6a7dacf625bcc7692cde308e80d1e7484e3c3b4
P. Mergey <gzm@gizmecano.net>
2019-05-07 07:32:47 +0200

    Add </catalog/account/wishlist.php>

---
+++ catalog/language/fr-FR/account/wishlist.php
@@ -0,0 +1,31 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Liste de souhaits';
+// Text
+$_['text_account'] = 'Compte';
+$_['text_instock'] = 'Disponible';
+$_['text_wishlist'] = 'Liste de souhaits (%s)';
+$_['text_login'] = 'Vous devez vous <a href="%s">connecter</a> ou <a href="%s">créer un compte</a> pour sauvegarder <a href="%s">%s</a> dans votre <a href="%s">liste de souhaits</a> !';
+$_['text_success'] = 'Vous avez ajouté <a href="%s">%s</a> à votre <a href="%s">liste de souhaits</a> avec succès !';
+$_['text_remove'] = 'Vous avez modifié votre liste de souhaits avec succès !';
+$_['text_empty'] = 'Votre liste de souhaits est vide.';
+// Column
+$_['column_image'] = 'Image';
+$_['column_name'] = 'Nom du produit';
+$_['column_model'] = 'Référence';
+$_['column_stock'] = 'Stock';
+$_['column_price'] = 'Prix unitaire';
+$_['column_action'] = 'Action';
