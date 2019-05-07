5e242acd00ce71d29173c8bcfdff7a13c3d9e0e5
P. Mergey <gzm@gizmecano.net>
2019-05-07 07:28:26 +0200

    Add </catalog/account/newsletter.php>

---
+++ catalog/language/fr-FR/account/newsletter.php
@@ -0,0 +1,22 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = 'Inscription à la lettre d’informations';
+// Text
+$_['text_account'] = 'Compte';
+$_['text_newsletter'] = 'Lettre d’informations';
+$_['text_success'] = 'Votre inscription à la lettre d’informations a été modifiée avec succès !';
+// Entry
+$_['entry_newsletter'] = 'S’abonner';
