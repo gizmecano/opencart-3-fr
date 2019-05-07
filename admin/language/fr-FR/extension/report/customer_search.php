641ab870d94f3a740a6bc112fdeb93bd2b234537
P. Mergey <gzm@gizmecano.net>
2019-05-07 09:10:32 +0200

    Fix license header in </admin/extension/report>

--- admin/language/fr-FR/extension/report/customer_search.php
+++ admin/language/fr-FR/extension/report/customer_search.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/******************************************************************************
  * Copyright (C) 2019 P. Mergey
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,7 +11,7 @@
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *****************************************************************************/
 // Heading
 $_['heading_title'] = 'Rapport des recherches des clients';
 // Text
