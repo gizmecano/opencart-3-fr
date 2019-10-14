2a71febf3f575b27cc0e819109a48ac7f117ffbb
P. Mergey <gzm@gizmecano.net>
2019-10-14 10:10:29 +0200

    Replace wording expressing gender (_vous avez été_)

--- upload/catalog/language/fr-FR/account/logout.php
+++ upload/catalog/language/fr-FR/account/logout.php
@@ -15,6 +15,6 @@
 // Heading
 $_['heading_title'] = 'Déconnexion du compte';
 // Text
-$_['text_message'] = 'Vous avez été déconnecté de votre compte. Quitter votre ordinateur est désormais sûr. Votre panier a été enregistré, les produits qui s’y trouvent pourront être retrouvés lorsque vous vous connecterez à nouveau à votre compte.';
+$_['text_message'] = 'Votre compte a été déconnecté. Quitter votre ordinateur est désormais sûr. Votre panier a été enregistré, les produits qui s’y trouvent pourront être retrouvés lorsque vous vous connecterez à nouveau à votre compte.';
 $_['text_account'] = 'Compte';
 $_['text_logout'] = 'Se déconnecter';
