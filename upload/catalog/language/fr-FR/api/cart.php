58e7ba6af07a5946c042b1ada618d56781287669
P. Mergey <gzm@gizmecano.net>
2020-09-01 08:36:22 +0200

    Improve numerous warning strings (catalog\api)

--- upload/catalog/language/fr-FR/api/cart.php
+++ upload/catalog/language/fr-FR/api/cart.php
@@ -16,7 +16,7 @@
 $_['text_success'] = 'Votre panier d’achat a été modifié avec succès !';
 // Error
 $_['error_permission'] = 'Attention : vous n’avez pas l’autorisation d’accéder à l’interface de programmation (API) !';
-$_['error_stock'] = 'Les produits indiqués avec *** ne sont pas disponibles dans la quantité désirée ou ne sont actuellement pas en stock !';
-$_['error_minimum'] = 'Le montant minimum d’une commande pour %s est %s !';
-$_['error_store'] = 'Le produit ne peut pas être acheté dans la boutique que vous avez choisi !';
-$_['error_required'] = '%s est requis !';
+$_['error_stock'] = 'Attention : les produits indiqués avec *** ne sont pas disponibles dans la quantité désirée ou ne sont actuellement pas en stock !';
+$_['error_minimum'] = 'Attention : le montant minimum d’une commande pour %s est %s !';
+$_['error_store'] = 'Attention : le produit ne peut pas être acheté dans la boutique que vous avez choisi !';
+$_['error_required'] = 'Attention : %s est requis !';
