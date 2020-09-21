47b1e7880261813c3f4eb302002ffdc6d18cb985
P. Mergey <gzm@gizmecano.net>
2020-09-21 11:25:14 +0200

    Update preliminary license notices

--- upload/admin/language/fr-FR/common/footer.php
+++ upload/admin/language/fr-FR/common/footer.php
@@ -1,6 +1,6 @@
 <?php
 /******************************************************************************
- * Copyright (C) 2019 P. Mergey
+ * Copyright (C) 2019-2020 P. Mergey
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -10,7 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/#GPL>.
  *****************************************************************************/
 // Text
 $_['text_footer'] = '<a href="https://www.opencart.com/" target="_blank">OpenCart</a> © 2009-2017 Tous droits réservés<br /><a href="https://github.com/gizmecano/opencart-3-fr/" target="_blank">opencart-3-fr</a> © 2019 P. Mergey';
--- upload/admin/language/fr-FR/extension/fraud/ip.php
+++ upload/admin/language/fr-FR/extension/fraud/ip.php
@@ -1,6 +1,6 @@
 <?php
 /******************************************************************************
- * Copyright (C) 2019 P. Mergey
+ * Copyright (C) 2019-2020 P. Mergey
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -10,7 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/#GPL>.
  *****************************************************************************/
 // Heading
 $_['heading_title'] = 'Antifraude IP';
