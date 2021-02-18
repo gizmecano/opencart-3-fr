39ddd5896d53ec4d11b3cc0fcdeb46142c82f521
P. Mergey <gzm@gizmecano.net>
2021-02-18 15:05:08 +0100

    Revise payment file in <admin\extension\extension>

--- upload/admin/language/fr-FR/extension/extension/payment.php
+++ upload/admin/language/fr-FR/extension/extension/payment.php
@@ -17,7 +17,6 @@ $_['heading_title'] = 'Moyens de paiement';
 // Text
 $_['text_success'] = 'Les moyens de paiements ont été modifiés avec succès !';
 $_['text_list'] = 'Liste des moyens de paiements';
-$_['text_recommended'] = 'Solutions de paiement recommandées';
 // Column
 $_['column_name'] = 'Moyen de paiement';
 $_['column_status'] = 'Statut';
