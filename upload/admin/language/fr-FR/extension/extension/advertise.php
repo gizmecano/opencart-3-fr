e62a0967aa3e678716020772045195b43cd22672
P. Mergey <gzm@gizmecano.net>
2021-02-18 14:50:27 +0100

    Append new file in <admin\extension\extension>

---
+++ upload/admin/language/fr-FR/extension/extension/advertise.php
@@ -0,0 +1,24 @@
+<?php
+/******************************************************************************
+ * Copyright (C) 2019-2021 P. Mergey
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/#GPL>.
+ *****************************************************************************/
+// Heading
+$_['heading_title'] = "Annonces publicitaires";
+// Columns
+$_['column_name'] = "Nom de l’annonce publicitaire";
+$_['column_status'] = "Statut";
+$_['column_action'] = "Action";
+// Text
+$_['text_success'] = "Les annonces publicitaires ont été modifées avec succès !";
+// Error
+$_['error_adblock'] = "Il semble que vous utilisiez un bloqueur de publicité. Afin d’utiliser cette section dédiée aux annonces publicitaires, veuillez désactiver votre bloqueur de publicité pour votre panneau d’administration.";
