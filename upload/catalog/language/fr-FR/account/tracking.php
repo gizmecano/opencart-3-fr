f0aa867d652e80703d676e2b8f2028f5e542bff3
P. Mergey <gzm@gizmecano.net>
2022-04-28 13:52:52 +0200

    Update copyright dates

--- upload/catalog/language/fr-FR/account/tracking.php
+++ upload/catalog/language/fr-FR/account/tracking.php
@@ -1,6 +1,6 @@
 <?php
 /******************************************************************************
- * Copyright (C) 2019-2021 P. Mergey
+ * Copyright (C) 2019-2022 P. Mergey
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
