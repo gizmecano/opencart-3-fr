ac5893d23e21227afd41999f6f512f6ac154c9ec
P. Mergey <gzm@gizmecano.net>
2022-10-03 13:30:25 +0200

    Add new string in `mail\voucher.php`

--- upload/admin/language/fr-FR/mail/voucher.php
+++ upload/admin/language/fr-FR/mail/voucher.php
@@ -20,3 +20,4 @@ $_['text_from'] = 'Ce bon d’achat a été envoyé de la part de %s';
 $_['text_message'] = 'Avec le message suivant';
 $_['text_redeem'] = 'Pour échanger ce bon d’achat, notez bien le code de rachat qui est <b>%s</b>. Cliquer ensuite sur le lien ci-dessous et acheter le produit pour lequel vous souhaitez utiliser ce bon d’achat. Vous pouvez saisir le code du bon d’achat sur la page du panier avant de valider la commande.';
 $_['text_footer'] = 'Vous pouvez répondre à ce courrier dans le cas où vous auriez une question.';
+$_['text_sent'] = 'Le bon d’achat a été envoyé avec succès !';
