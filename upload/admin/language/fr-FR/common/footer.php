b0b7fc619cbeaa3208c0da62595afc5bbb052248
P. Mergey <gzm@gizmecano.net>
2023-05-23 09:55:05 +0200

    Update copyright dates

--- upload/admin/language/fr-FR/common/footer.php
+++ upload/admin/language/fr-FR/common/footer.php
@@ -1,6 +1,6 @@
 <?php
 /******************************************************************************
- * Copyright (C) 2019-2022 P. Mergey
+ * Copyright (C) 2019-2023 P. Mergey
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -13,5 +13,5 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/#GPL>.
  *****************************************************************************/
 // Text
-$_['text_footer'] = '<a href="https://www.opencart.com/" target="_blank">OpenCart</a> © 2009-2020 Tous droits réservés<br /><a href="https://github.com/gizmecano/opencart-3-fr/" target="_blank">opencart-3-fr</a> © 2019-2022 P. Mergey';
+$_['text_footer'] = '<a href="https://www.opencart.com/" target="_blank">OpenCart</a> © 2009-2020 Tous droits réservés<br /><a href="https://github.com/gizmecano/opencart-3-fr/" target="_blank">opencart-3-fr</a> © 2019-2023 P. Mergey';
 $_['text_version'] = 'Version %s';
